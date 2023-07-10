3e6680e81f9b022fba8c4d16aa421067a69d6f3d
Katlego-Sekoele <sekoelekatlego@gmail.com>
2023-07-10 22:15:08 +0200

    socket communication boilerplate

--- 1_player/connect.js
+++ 1_player/connect.js
@@ -1,5 +1,35 @@
-function connectToServer()
-{
-    console.log("Hier");
-    window.location.href("quiz.html");
+import "./socket.io/client-dist/socket.io.js";
+
+const SERVER_URL = 'http://localhost:3001'
+let socket // manager connects to socket server
+
+document.getElementById('connectBtn').onclick = function (){
+    // establish connection to socket server
+    socket = io.connect(SERVER_URL);
 }
+
+socket.on("connect", () => {
+    // connect event
+    const engine = socket.io.engine;
+
+    engine.on("packet", ({ type, data }) => {
+        // called for each packet received
+    });
+
+    engine.on("packetCreate", ({ type, data }) => {
+        // called for each packet sent
+    });
+
+});
+
+// disconnect event
+socket.on("disconnect", () => {
+
+});
+
+socket.on("generic_event", (data) => {
+//     TODO: decide on events with server team
+});
+
+// send payload to server and invoke another_generic_event on server side
+// socket.emit("another_generic_event", payload)
