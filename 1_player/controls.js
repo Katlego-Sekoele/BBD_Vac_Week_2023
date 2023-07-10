95bfc7bbd364798f0066ea996609542051152834
Siphesihle <percysiphe@gmail.com>
2023-07-10 15:49:52 +0200

    Controls

---
+++ 1_player/controls.js
@@ -0,0 +1,15 @@
+document.getElementById("up").addEventListener('click', () => {
+    alert("0")
+});
+
+document.getElementById("left").addEventListener('click', () => {
+    alert("270")
+});
+
+document.getElementById("down").addEventListener('click', () => {
+    alert("180")
+});
+
+document.getElementById("right").addEventListener('click', () => {
+    alert("90")
+});
