33e3c611fd715ca6de4d9d8fd41069f4cfb4a126
Gerni <gerni.visser@gmail.com>
2023-07-11 20:59:46 +0200

    touch control

--- 1_player/controls.js
+++ 1_player/controls.js
@@ -1,24 +1,51 @@
 const SERVER_URL = "http://localhost:3000";
 
 const socket = io.connect(SERVER_URL);
-var numberOfMoves = 5;
 
-function saveDir(e, dir) {
+function sendMove(e, dir, speed) {
     e.preventDefault();
-    sendDir(dir)
-    numberOfMoves --
-    console.log(dir + ": number of moves left " + numberOfMoves);
-
-    if(numberOfMoves <= 0){
-        numberOfMoves = 5
-        window.location.assign("quiz.html");
-    }
+    socket.emit("move_ball", dir, speed)
+    window.location.assign("quiz.html");
 }
 
-function sendDir(dir){
-    socket.emit("move_ball", dir)
-}
+// Variables to store initial touch position and timestamp
+let startX, startY, startTime;
 
+// Constants for angle calculation
+const RAD_TO_DEG = 180 / Math.PI;
+
+// Event listener for touch start
+document.addEventListener('touchstart', function(event) {
+  // Store initial touch position and timestamp
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+  startTime = Date.now();
+});
+
+// Event listener for touch end
+document.addEventListener('touchend', function(event) {
+  // Calculate drag distance
+  const endX = event.changedTouches[0].clientX;
+  const endY = event.changedTouches[0].clientY;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+
+  // Calculate angle in degrees
+  const angle = Math.atan2(distanceY, distanceX) * RAD_TO_DEG;
+
+  // Calculate drag speed
+  const endTime = Date.now();
+  const duration = endTime - startTime;
+  const speed = Math.sqrt((distanceX * distanceX + distanceY * distanceY)) / duration;
+
+  // Output the calculated values
+  console.log('Angle: ' + angle + ' degrees');
+  console.log('Speed: ' + speed + ' pixels per millisecond');
+
+  sendMove(e, angle, speed);
+});
+
+/*
 document.getElementById("up").addEventListener('click', (e) => {
     saveDir(e, 1);
 });
@@ -34,3 +61,4 @@ document.getElementById("down").addEventListener('click', (e) => {
 document.getElementById("right").addEventListener('click', (e) => {
     saveDir(e, 91);
 });
+*/
