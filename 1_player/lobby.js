d078ec6d310eab7ecd219e96064215f5a0eaac51
Mignon Erasmus <mignon.erasmus13@gmail.com>
2023-07-11 10:12:54 +0200

    js moved from lobby.html to lobby.js

--- 1_player/lobby.js
+++ 1_player/lobby.js
@@ -14,3 +14,24 @@ function connectToServer(gameSettings)
 {
     window.location.assign("quiz.html");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var queuedPlayers = [
+        "Player A",
+        "Player B",
+        "Player C",
+        "Player D"
+    ];
+      var queuedPlayerListElement = document.getElementById("player-list");
+      
+        // Function to populate the list with players
+        function populatePlayerList() {
+            queuedPlayers.forEach(function(player) {
+                var listItem = document.createElement("li");
+                listItem.textContent = player;
+                queuedPlayerListElement.appendChild(listItem);
+            });
+        }
+        // Call the function to populate the list
+        populatePlayerList();
+});
