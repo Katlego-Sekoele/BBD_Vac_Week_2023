91d7f7422aa1a85ba2bcd931974a791cd51de701
Verushan <verushannaidoo@gmail.com>
2023-07-11 22:19:24 +0200

    Improved camera accuracy and modularity

--- server/src/camera/camera.js
+++ server/src/camera/camera.js
@@ -1,86 +1,130 @@
-// Access video, canvas, and coordinate elements
-const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
+const video = document.getElementById("video");
 const context = canvas.getContext("2d", { willReadFrequently: true });
 
-// Colors
-const Player1 = [170, 239, 61];// Light Green
-const Player2 = [0, 175, 203]; // Light Blue
-const Player3 = [246, 21, 144]; // Light pink/purple
-const Player4 = [225, 255, 0];  // Light yellow
-const Ball = [225, 0, 0]; // Red
+// Ball colour followed by player colours
+const CONFIG = [
+    [200, 0, 0],
+    [170, 239, 61],
+    [0, 175, 203],
+    [246, 21, 144],
+    [225, 225, 0]
+];
 
-function absoluteDifference(pixel, color) {
-  let ans = 0
-  for (let i = 0; i < 3; i++) {
-    const diff = Math.abs(pixel[i] - color[i]);
-    ans += diff;
-  };
+const THRESHOLD = 75;
+const NO_CLASS = -1;
 
-  return ans;
+function getAveragePosition(pos1, pos2) {
+    result = [0, 0];
+    result[0] = Math.floor((pos1[0] + pos2[0]) / 2);
+    result[1] = Math.floor((pos1[1] + pos2[1]) / 2);
+    return result;
 }
 
-function updatePositions(pixel, x, y, positions) {
-  let threshold = 65;
-
-  if (absoluteDifference(pixel, Ball) <= threshold) {
-    positions.ball = [x, y];
-  } else if (absoluteDifference(pixel, Player1) <= threshold) {
-    positions.player1 = [x, y];
-  }
-  else if (absoluteDifference(pixel, Player2) <= threshold) {
-    positions.player2 = [x, y];
-  } else if (absoluteDifference(pixel, Player3) <= threshold) {
-    positions.player3 = [x, y];
-  } else if (absoluteDifference(pixel, Player4) <= threshold) {
-    positions.player4 = [x, y];
-  }
+function getColourSimilarity(pixel, color) {
+    let ans = Math.abs(pixel[0] - color[0]);
+    ans += Math.abs(pixel[1] - color[1]);
+    ans += Math.abs(pixel[2] - color[2]);
+    return ans;
 }
 
-function getCameraPermission() {
-  // Request access to the webcam
-  navigator.mediaDevices
+function getColourClass(pixelValue) {
+    for (let i = 0; i < CONFIG.length; ++i) {
+        if (getColourSimilarity(pixelValue, CONFIG[i]) <= THRESHOLD) {
+            return i;
+        }
+    }
+
+    return NO_CLASS;
+}
+
+function assignMatchingColour(image, index, positions) {
+    let red = image[index];
+    let green = image[index + 1];
+    let blue = image[index + 2];
+
+    let x = (index / 4) % canvas.width;
+    let y = Math.floor((index / 4) / canvas.width);
+    let pixelLocation = [x, y];
+
+    let pixelValue = [red, green, blue];
+    let colourClass = getColourClass(pixelValue);
+
+    if (colourClass == NO_CLASS)
+        return;
+
+    positions[colourClass] = pixelLocation;
+}
+
+
+function getObjectPositions() {
+    const image = context.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    let bottomRightPositions = new Array(CONFIG.length).fill([-1, -1]);
+    let topLeftPositions = new Array(CONFIG.length).fill([-1, -1]);
+
+    // Loop through each pixel of the canvas
+    for (let i = 0; i < image.length; i += 4) {
+        assignMatchingColour(image, i, bottomRightPositions);
+    }
+
+    for (let i = image.length - 4; i >= 0; i -= 4) {
+        assignMatchingColour(image, i, topLeftPositions);
+    }
+
+    const finalPositions = new Array(CONFIG.length).fill([-1, -1]);
+
+    for (let i = 0; i < CONFIG.length; ++i) {
+        finalPositions[i] = getAveragePosition(bottomRightPositions[i], topLeftPositions[i]);
+    }
+
+    context.beginPath();
+    context.fillStyle = "green";
+    // This will put a dot at the centre of player 2's colour
+    context.arc(finalPositions[2][0], finalPositions[2][1], 5, 0, Math.PI * 2, true);
+    context.fill();
+
+    jsonObj = {
+        ball: finalPositions[0]
+    };
+
+    cones = {};
+
+    for (let i = 1; i < CONFIG.length; ++i) {
+        let key = "Player" + i.toString();
+        cones[key] = finalPositions[i];
+    }
+
+    jsonObj.cones = cones;
+    jsonObj.dimension = [canvas.width, canvas.height];
+
+    return JSON.stringify(jsonObj);
+}
+
+// Request access to the webcam
+navigator.mediaDevices
     .getUserMedia({ video: true })
     .then((stream) => {
-      video.srcObject = stream;
-      video.play();
+        video.srcObject = stream;
+        video.play();
     })
     .catch((error) => {
-      console.error("Error accessing the webcam: ", error);
+        console.error("Error accessing the webcam: ", error);
     });
-}
 
-// Function to track the ball
+// Use this function for simply returning the JSON object
+// function getMap() {
+//     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     return getObjectPositions();
+// }
+
+// This implementation will redraw the image with highlighting the found ball with a green dot
 function getMap() {
-  return trackBallPosition();
-}
+    function draw() {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        getObjectPositions();
+        requestAnimationFrame(getMap);
+    }
 
-// Identify the ball position
-function trackBallPosition() {
-  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-  const data = imageData.data;
-
-  const positions = {
-    ball: [-1, -1],
-    player1: [-1, -1],
-    player2: [-1, -1],
-    player3: [-1, -1],
-    player4: [-1, -1],
-    dimensions: [canvas.width, canvas.height]
-  };
-
-  // Loop through each pixel of the canvas
-  for (let i = 0; i < data.length; i += 4) {
-    const red = data[i];
-    const green = data[i + 1];
-    const blue = data[i + 2];
-
-    // Check if the pixel represents the ball color (adjust the values as per your ball color)
-    const x = (i / 4) % canvas.width;
-    const y = Math.floor(i / 4 / canvas.width);
-    let pixel = [red, green, blue];
-    updatePositions(pixel, x, y, positions);
-  }
-
-  return JSON.stringify(positions);
+    draw();
 }
