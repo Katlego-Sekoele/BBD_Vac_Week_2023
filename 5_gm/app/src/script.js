1ed57a417e71ad21365615936eabf2f6bed40252
Langeveldt, CH, Mr [23632135@sun.ac.za] <23632135@sun.ac.za>
2023-07-11 09:49:29 +0200

    testing things

---
+++ 5_gm/app/src/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myButton").addEventListener("click", function() {
+      // Redirect to the second page
+      window.location.href = "secondpage.html";
+    });
+  });
