ba09a9d7b775f2aac665c5fb12dd4eb92d7c7b43
Sama1am <svsvnst@gmail.com>
2023-07-11 09:29:21 +0200

    ball controller
    -connects to ball
    -moves ball

--- 4_sphero/sphero_ball/controls.js
+++ 4_sphero/sphero_ball/controls.js
@@ -1,8 +1,34 @@
-const sphero = require("sphero");
-var orb = sphero("/dev/rfcomm0"); // change port accordingly
+const { Scanner, Utils } = require('spherov2.js');
 
-console.log(orb);
+let ball;
 
-orb.connect(function() {
-    console.log("connected");
-});
+//connects to the ball
+const connectBall = async () =>{
+     ball = await Scanner.findSpheroMini();
+    
+    if (!sphero){
+        logToMain({type:"connection", message:"error"});
+    } else {
+        logToMain({type:"connection", message:"success"});
+    }
+}
+
+//moves the ball 
+const moveBall = async(speed, angle) => {
+    if (!sphero){
+        logToMain({type:"move", message:"error"});
+        return;
+    }
+
+    angle = 90;
+    speed = 100;
+
+    ball.roll(speed, angle);
+
+    let dir = [speed, angle];
+    logToMain({type:"move", message: dir});
+}
+
+const logToMain = async (JSONObject) => {
+    console.log(JSONObject);
+}
