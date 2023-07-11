452dd24141ace498bc5b33c5f98154cb364d4cc5
Daviddo071 <108659499+Daviddo071@users.noreply.github.com>
2023-07-11 15:13:35 +0200

    Talks to server and ball works
    @daniel and @sam also worked on it with me

--- 4_sphero/sphero_ball/python/main.py
+++ 4_sphero/sphero_ball/python/main.py
@@ -1,6 +1,10 @@
 import socketio
+from spherov2 import scanner
+from spherov2.sphero_edu import SpheroEduAPI
 
 sio = socketio.Client()
+toy = None
+
 
 @sio.event
 def connect():
@@ -12,13 +16,37 @@ def disconnect():
     print('Disconnected from server')
 
 @sio.event
-def message(data):
-    print('Received message:', data)
+def on_ball_control(data):
+    data = int(data) + 1
+    print(data)
+    print(type(data))
+    # input()
+    if toy==None:
+        print("No ball")
+    try:
+        with SpheroEduAPI(toy) as api:
+            speed = 255
+            api.set_speed(0)
+            print('rolling')
+            api.roll(data, 255, 1)
+    except:
+        print("Error")
+        pass
+    print("Done")
 
+@sio.event
 def sendMessage():
-    sio.emit('message', "Olla :)")
+    print("Sending")
+    sio.emit('move_ball', '1')
+    print("Sent")
 
 
 if __name__ == '__main__':
+    toy = scanner.find_BB8()
+    if toy == None:
+        print("No toy found")
+    else:
+        print(f"Toy: {toy.name} found")
+
     sio.connect('https://server-bbd-vac-week.onrender.com')
     sio.wait()
