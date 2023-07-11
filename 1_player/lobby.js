dcda00d6aa2c66d9f41d1c78c982dc7133593fa3
Gerni <gerni.visser@gmail.com>
2023-07-11 09:58:26 +0200

    start game redirect

--- 1_player/lobby.js
+++ 1_player/lobby.js
@@ -1,8 +1,16 @@
-const myTimeout = setTimeout(connectToServer, 5000);
+import "./socket.io/client-dist/socket.io.js";
+
+const SERVER_URL = "http://localhost:3001";
+let socket; // manager connects to socket server
+
+socket = io.connect(SERVER_URL);
+
+socket.on("StartGame", (res) => {
+    connectToServer(res)
+})
 
 
-function connectToServer()
+function connectToServer(gameSettings)
 {
-    console.log("abc");
     window.location.assign("quiz.html");
 }
