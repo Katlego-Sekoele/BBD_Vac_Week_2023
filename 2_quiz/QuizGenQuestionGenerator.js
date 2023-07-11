09b7d1e286ca096a11493b37a9e67eb771cdb3f9
Joseph Kimathi <24032220@sun.ac.za>
2023-07-11 09:46:41 +0200

    exported public functions

--- 2_quiz/QuizGenQuestionGenerator.js
+++ 2_quiz/QuizGenQuestionGenerator.js
@@ -2,7 +2,7 @@
 // var question, options, ansIndex;
 
 // Quiz refers to question and set of answers
-function getQuiz(){
+export function getQuiz() {
     var question = 'How many planets are there in our solar system?'; // TODO: change to database call                
     var options = ['6', '7', '8', '9']; // TODO: change to database call
     var ansIndex = 2; // TODO: change to database call
@@ -20,8 +20,8 @@ function getQuiz(){
     return quiz;
 }
 
-function checkAnswer(searchQuestion, answer){
-    if(!searchQuestion.equals(question)){
+export function checkAnswer(searchQuestion, answer) {
+    if (!searchQuestion.equals(question)) {
         // If the question is not in the cache then populate the cache with it
         var question; // TODO: Change this to a database call
         var options; // TODO: Change this to a database call
@@ -29,17 +29,17 @@ function checkAnswer(searchQuestion, answer){
     }
 
 
-    if(answer == ansIndex)
+    if (answer == ansIndex)
         return true;
     return false;
 }
 
 // Might be removed
-function generateQuestions(numQ){
+export function generateQuestions(numQ) {
     // TODO: Api that generates questions
     var questions = ['How many planets are there in our solar system?',
-                    'Which Italian explorer discovered North America, thinking that it was India?'
-                    ];
+        'Which Italian explorer discovered North America, thinking that it was India?'
+    ];
     var options = [
         ['6', '7', '8', '9'],
         ['Marco Polo', 'Mansa Musa', 'Christopher Colombus', 'Galileo Galilei']
