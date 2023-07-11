a7c57a02d2f3fa4a4bc611a407ec9baf563e1b40
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-07-11 10:16:46 +0200

    Fixed node errors

--- 2_quiz/QuizGenQuestionGenerator.js
+++ 2_quiz/QuizGenQuestionGenerator.js
@@ -2,50 +2,44 @@
 // var question, options, ansIndex;
 
 // Quiz refers to question and set of answers
-export function getQuiz() {
-    var question = 'How many planets are there in our solar system?'; // TODO: change to database call                
-    var options = ['6', '7', '8', '9']; // TODO: change to database call
-    var ansIndex = 2; // TODO: change to database call
-    var quiz = {
-        "Question": question,
-        "Answers": [
-            options[0],
-            options[1],
-            options[2],
-            options[3]
-        ],
-        "Correct_Answer_Index": ansIndex
-    };
+function getQuiz() {
+  var question = "How many planets are there in our solar system?"; // TODO: change to database call
+  var options = ["6", "7", "8", "9"]; // TODO: change to database call
+  var ansIndex = 2; // TODO: change to database call
+  var quiz = {
+    Question: question,
+    Answers: [options[0], options[1], options[2], options[3]],
+    Correct_Answer_Index: ansIndex,
+  };
 
-    return quiz;
+  return quiz;
 }
 
-export function checkAnswer(searchQuestion, answer) {
-    if (!searchQuestion.equals(question)) {
-        // If the question is not in the cache then populate the cache with it
-        var question; // TODO: Change this to a database call
-        var options; // TODO: Change this to a database call
-        var ansIndex = -1; // TODO: Change this to a database call
-    }
+function checkAnswer(searchQuestion, answer) {
+  if (!searchQuestion.equals(question)) {
+    // If the question is not in the cache then populate the cache with it
+    var question; // TODO: Change this to a database call
+    var options; // TODO: Change this to a database call
+    var ansIndex = -1; // TODO: Change this to a database call
+  }
 
-
-    if (answer == ansIndex)
-        return true;
-    return false;
+  if (answer == ansIndex) return true;
+  return false;
 }
 
 // Might be removed
-export function generateQuestions(numQ) {
-    // TODO: Api that generates questions
-    var questions = ['How many planets are there in our solar system?',
-        'Which Italian explorer discovered North America, thinking that it was India?'
-    ];
-    var options = [
-        ['6', '7', '8', '9'],
-        ['Marco Polo', 'Mansa Musa', 'Christopher Colombus', 'Galileo Galilei']
-    ];
+function generateQuestions(numQ) {
+  // TODO: Api that generates questions
+  var questions = [
+    "How many planets are there in our solar system?",
+    "Which Italian explorer discovered North America, thinking that it was India?",
+  ];
+  var options = [
+    ["6", "7", "8", "9"],
+    ["Marco Polo", "Mansa Musa", "Christopher Colombus", "Galileo Galilei"],
+  ];
 
-    var answerIndexes = [2, 2];
+  var answerIndexes = [2, 2];
 }
 
 // TEST CODE START
@@ -54,3 +48,5 @@ console.log(testQuiz.Correct_Answer_Index);
 console.log(testQuiz.Question);
 console.log(testQuiz.Answers);
 // TEST CODE END
+
+module.exports = { generateQuestions, getQuiz };
--- 2_quiz/test.js
+++ 2_quiz/test.js
@@ -1,5 +1,5 @@
-import { getQuiz } from "./QuizGenQuestionGenerator.js";
-var testQuiz = getQuiz();
+const QuizGen = require("./QuizGenQuestionGenerator");
+var testQuiz = QuizGen.getQuiz();
 console.log(testQuiz.Correct_Answer_Index);
 console.log(testQuiz.Question);
 console.log(testQuiz.Answers);
