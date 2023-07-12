c2328350a2271bb383505a669dbd0e7d960b753a
Jenna_Dunford <Jenna.Dunford@bbd.co.za>
2023-07-12 09:46:05 +0200

    Map Things and Styling

--- 5_gm/app/src/MapStuff/mapMain.js
+++ 5_gm/app/src/MapStuff/mapMain.js
@@ -3,10 +3,17 @@ import Two from '../../node_modules/two.js/src/two.js';
 
   // Function to create the visual map using Two.js
 export function createMap(data) {
+
+    const containerWidth = document.getElementById("pixel_map_container").offsetWidth;
+    const containerHeight = document.getElementById("pixel_map_container").offsetHeight;
+    const cellWidth = containerWidth / data[0].length;
+    const cellHeight = containerHeight / data.length;
   const two = new Two({
-    width: data[0].length * 10, // Adjust the width based on the number of columns
-    height: data.length * 10, // Adjust the height based on the number of rows
-  }).appendTo(document.getElementById('draw-shapes'));
+    //width: data[0].length * 5, // Adjust the width based on the number of columns
+    //height: data.length * 5, // Adjust the height based on the number of rows
+    width: containerWidth*2,
+    height: containerHeight
+  }).appendTo(document.getElementById('pixel_map_container'));
 
   // Loop through the rows of the 2D array
   for (let i = 0; i < data.length; i++) {
@@ -17,17 +24,24 @@ export function createMap(data) {
       const value = row[j];
 
       // Calculate the position of the shape
-      const x = j * 10 + 5;
-      const y = i * 10 + 5;
+      // const x = j * 5 + 2;
+      // const y = i * 5 + 2;
+
+      const x = j * cellWidth + cellWidth / 2;
+      const y = i * cellHeight + cellHeight / 2;
+
+      // const x = j * cellSize + cellSize;
+      // const y = i * cellSize + cellSize;
       const circle = null;
       const triangle = null;
       const square = null;
       // Create a shape based on the value in the 2D array
       if (value === 'W') { // 0 is a square (empty space)
-        //const square = two.makePolygon(x, y, 10, 4);
-        //square.fill = 'grey';
+        const square = two.makePolygon(x, y, 10, 4);
+        square.fill = 'grey';
       } else if (value === '1' || value === '2' || value === '3' || value === '4') { // 1 is a cone
         const triangle = two.makePolygon(x, y, 10, 3);
+        console.log("MAKE TRIANGLE");
         triangle.fill = 'orange';
       } else if (value === 'B') { // 2 is a circle (ball)
         const circle = two.makeCircle(x, y, 10);
@@ -42,6 +56,46 @@ export function createMap(data) {
   // Render the scene
   two.update();
 }
+
+export function createMap2(mapData){
+  const containerWidth = document.getElementById("pixel_map_container").offsetWidth;
+    const containerHeight = document.getElementById("pixel_map_container").offsetHeight;
+    const cellSize = Math.min(containerWidth / mapData[0].length, containerHeight / mapData.length);
+
+    // Create the Two.js instance
+    const two = new Two({
+      width: cellSize * mapData[0].length,
+      height: cellSize * mapData.length,
+      autostart: true
+    }).appendTo(document.getElementById("pixel_map_container"));
+
+    // Create shapes based on the map data
+    for (let i = 0; i < mapData.length; i++) {
+      for (let j = 0; j < mapData[i].length; j++) {
+        let value = '';
+        mapData[i] = value;
+        const circle = null;
+        const triangle = null;
+        const square = null;
+
+        const x = j * cellSize + cellSize / 2;
+        const y = i * cellSize + cellSize / 2;
+        // Create a shape based on the value in the 2D array
+        if (value === 'W') { // 0 is a square (empty space)
+        square = two.makePolygon(x, y, 10, 4);
+        square.fill = 'grey';
+        } else if (value === '1' || value === '2' || value === '3' || value === '4') { // 1 is a cone
+          triangle = two.makePolygon(x, y, 10, 3);
+          triangle.fill = 'orange';
+        } else if (value === 'B') { // 2 is a circle (ball)
+          circle = two.makeCircle(x, y, 10);
+          circle.fill = 'red';
+        }
+        two.add(circle || triangle || square);
+        }
+      }
+      two.update();
+    }
 
 // Call the createMap function with the mapData array
 //createMap(mapData);
--- 5_gm/app/src/main.js
+++ 5_gm/app/src/main.js
@@ -1,4 +1,5 @@
 //import "./socket.io/client-dist/socket.io.js";
+import {createMap} from "./MapStuff/mapMain.js";
 // for navigation purposes
 const startGameContainer = document.getElementById('start-game-container');
 const setUpGameContainer = document.getElementById('set-up-game-container');
@@ -10,8 +11,6 @@ const mapContainer = document.getElementById('map-container');
 document.getElementById("homescreen-start-btn").addEventListener("click", showSetUpGameContainer);
 document.getElementById("nextbutton").addEventListener("click", showQRScreenMainBox);
 document.getElementById("start-game-btn").addEventListener("click", showQuestionPageMainBox);
-document.getElementById("go-to-map-btn").addEventListener("click", showMapContainer);
-document.getElementById("back-to-game-btn").addEventListener("click", showQuestionPageMainBox);
 
 // Socket.io stuff
 const SERVER_URL = "http://localhost:3000";
@@ -62,10 +61,79 @@ socket.on('updated_map', (data) => {
 	createMap(data);
 });
 
+socket.on('current_players', (currentPlayers) => {
+    players = currentPlayers;
+    updateScoreboard();
+});
 
-//nav functions
-function showStartGameContainer() {
-    startGameContainer.style.display = 'block';
+socket.on("lobby_code", lobbyCode => {
+    currentLobbyCode = lobbyCode;
+    // TODO: document.getElementById("lobby_code").innerText = lobbyCode;
+});
+
+socket.on('duel', (playerControllingTheBall) => {
+    document.getElementById("who_controlling_ball").innerText = playerControllingTheBall.username +" is controlling the ball";
+    // TODO: get the info using the camera now to cause map updates
+});
+socket.on('done_duel', () => {
+    document.getElementById("who_controlling_ball").innerText = "No one is controlling the ball";
+});
+
+
+socket.on('on_next_question', (question) => {
+    //update the question and answers
+    document.getElementById("question_holder").innerText = question.question;
+    document.getElementById("answer_1").innerText = question.answers[0]; //TODO: idk how the question object is structured
+    document.getElementById("answer_2").innerText = question.answers[1];
+    document.getElementById("answer_3").innerText = question.answers[2];
+    document.getElementById("answer_4").innerText = question.answers[3];
+    
+    document.getElementById("answer_1").style.backgroundColor = "pink";
+    document.getElementById("answer_2").style.backgroundColor = "pink";
+    document.getElementById("answer_3").style.backgroundColor = "pink";
+    document.getElementById("answer_4").style.backgroundColor = "pink";
+});
+
+// TODO: add the code so when the next question button is pressed then the socket.emit is called
+document.getElementById("next_question_btn").onclick = () => {
+	socket.emit("make_next_question"); //I don't think there is data to this event
+};
+
+//TODO: add the code so when the 'evaluate' button is pressed then the socket.emit is called
+document.getElementById("evaluate_btn").onclick = () => {
+    socket.emit("evaluate");
+};
+
+socket.on('on_correct_answer', (indexOfCorrectAnswer) => {
+    document.getElementById("answer_" + (indexOfCorrectAnswer + 1)).style.backgroundColor = "green";
+    updateScoreboard();
+});
+
+
+function updateScoreboard() {
+    for (var i = 1; i < 9; i++) { // this is added so that if the number of players decreases then the others will be removed
+        // Update div content with usernames and score
+        var divUsername = document.getElementById("username_" + (i + 1));
+        divUsername.innerText = "";
+        var divScore = document.getElementById("score_" + (i + 1));
+        divScore.innerText = ""; 
+      }
+
+    for (var i = 1; i < players.length; i++) {
+        var player = players[i];
+  
+        // Update div content with usernames and score
+        var divUsername = document.getElementById("username_" + (i + 1));
+        divUsername.innerText = username;
+        var divScore = document.getElementById("score_" + (i + 1));
+        divScore.innerText = player.score; 
+      }
+    }
+    
+    
+    //nav functions
+    function showStartGameContainer() {
+        startGameContainer.style.display = 'block';
     setUpGameContainer.style.display = 'none';
     qrScreenMainBox.style.display = 'none';
     questionPageMainBox.style.display = 'none';
@@ -92,6 +160,7 @@ function showQRScreenMainBox() {
 
 // Function to show the question page main box container and hide the rest
 function showQuestionPageMainBox() {
+    socket.emit('generate_initial_map', 4);
     startGameContainer.style.display = 'none';
     setUpGameContainer.style.display = 'none';
     qrScreenMainBox.style.display = 'none';
